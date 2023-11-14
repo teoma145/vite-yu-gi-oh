05845592e749666ccef32f1e7512a8c0b1465e74
teoma145 <matteodark145@yahoo.it>
2023-11-14 18:01:06 +0100

    fixstore.js

--- src/assets/data/store.js
+++ src/assets/data/store.js
@@ -3,8 +3,8 @@ import { reactive } from "vue";
 
 
 export const store = reactive({
-  apiUrl: "https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0/",
-  archetypeUrl:"https://db.ygoprodeck.com/api/v7/archetypes.php",
+  
+  
   ArchetypesList:[],
   CardList: [],
 });
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -10,7 +10,7 @@
 
 <script>
   import { store } from '../assets/data/store.js';
-export default {
+  export default {
     name:'SearchBar',
     data () {
         
